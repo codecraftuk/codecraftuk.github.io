d48e5850338e4cf0345dbfa86eb1ba6373e3b0e3
garyfleming <gary.fleming@gmail.com>
2014-01-16 22:52:26 +0000

    Fixing routing to CSS in poole

--- app.rb
+++ app.rb
@@ -7,7 +7,12 @@ module CodeCraft
     end
 
     get "/events/?*" do
-        poole_blog(request.path) {404}
+      poole_blog(params[:splat]) {404}
+    end
+
+    get "/public/?*" do
+      file_path = File.join(File.dirname(__FILE__), 'poole/public', params[:splat])
+      serve_data(file_path)
     end
 
     get "/tmp" do
@@ -20,7 +25,7 @@ module CodeCraft
         @current_menu = "poole"
         @title = "CodeCraft"
       
-        file_path = File.join(File.dirname(__FILE__), 'poole/_site',  path.gsub('/events',''))
+        file_path = File.join(File.dirname(__FILE__), 'poole/_site',  path)
         file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i  
       
         serve_data(file_path)
@@ -31,9 +36,11 @@ module CodeCraft
           file = File.open(file_path, "rb")
           contents = file.read
           file.close
-      
-          if (file_path.include?('.xml') || file_path.include?('.css'))
+
+          if (file_path.include?('.xml'))
             erb contents, :content_type => 'text/xml'
+          elsif (file_path.include?('.css'))
+            erb contents, :content_type => 'text/css'
           else
             erb contents
           end
